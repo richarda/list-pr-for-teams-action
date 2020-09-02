25d9bc1b474b7cc1d4388601dc5710c478c06111
Richard Adams <204173+richarda@users.noreply.github.com>
2020-09-01 21:36:25 -0400

    fix error

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-    console.log('Begining PR lookup')
-    const time = (new Date()).toTimeString();
+  console.log("Begining PR lookup");
+  const time = new Date().toTimeString();
+} catch (error) {
+  core.setFailed(error.message);
 }
